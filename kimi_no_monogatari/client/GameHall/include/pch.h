238e7aadeae0a57248483dbcc9ec44bf325420b6
dummyedu <dummyedu@gmail.com>
2011-11-27 09:10:28 +0800

    add some type defines.

--- kimi_no_monogatari/client/GameHall/include/pch.h
+++ kimi_no_monogatari/client/GameHall/include/pch.h
@@ -5,10 +5,23 @@
 
 #include <assert.h>
 
+// TODO classify the defines and functions and move into different files.
+
 #ifdef _DEBUG
 #define ASSERT(x) assert (x);
 #else /* _DEBUG */
 #define ASSERT(x) ;
 #endif /* _DEBUG */
+
+// define the types just for simplify the typeing. so 
+// char, unsigned char in function signature is acceptable.
+// any other idea?
+
+typedef unsigned char u8;
+typedef signed char s8;
+typedef unsigned short u16;
+typedef signed short s16;
+typedef unsigned int u32;
+typedef signed int s32;
 
 #endif /* _PCH_H_ */
