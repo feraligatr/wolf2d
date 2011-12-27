209774001efd6e63db9f8c5441faa4c143e5ec6c
dummyedu <dummyedu@gmail.com>
2011-12-27 12:49:56 +0800

    add AABB and Plane.

--- kimi_no_monogatari/client/math/trMath.cpp
+++ kimi_no_monogatari/client/math/trMath.cpp
@@ -1,3 +1,37 @@
+/*
+-----------------------------------------------------------------------------
+This source file is part of OGRE
+(Object-oriented Graphics Rendering Engine)
+For the latest info, see http://www.ogre3d.org/
+
+Copyright (c) 2000-2009 Torus Knot Software Ltd
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+-----------------------------------------------------------------------------
+*/
+// This file is based on material originally from:
+// Geometric Tools, LLC
+// Copyright (c) 1998-2010
+// Distributed under the Boost Software License, Version 1.0.
+// http://www.boost.org/LICENSE_1_0.txt
+// http://www.geometrictools.com/License/Boost/LICENSE_1_0.txt
+
 #include "pch/pch.h"
 
 #include "trMath.h"
@@ -637,5 +671,95 @@ namespace tree
 		m[3][0] = 0; m[3][1] = 0; m[3][2] = 0; m[3][3] = 1;
 	}
 
+	const AABB AABB::BOX_NULL;
+	const AABB AABB::BOX_INFINITE(AABB::EXTENT_INFINITE);
+
+	Plane::Plane ()
+	{
+		normal = Vec3::ZERO;
+		d = 0.0;
+	}
+	//-----------------------------------------------------------------------
+	Plane::Plane (const Plane& rhs)
+	{
+		normal = rhs.normal;
+		d = rhs.d;
+	}
+	//-----------------------------------------------------------------------
+	Plane::Plane (const Vec3& rkNormal, Real fConstant)
+	{
+		normal = rkNormal;
+		d = -fConstant;
+	}
+	//---------------------------------------------------------------------
+	Plane::Plane (Real a, Real b, Real c, Real _d)
+		: normal(a, b, c), d(_d)
+	{
+	}
+	//-----------------------------------------------------------------------
+	Plane::Plane (const Vec3& rkNormal, const Vec3& rkPoint)
+	{
+		redefine(rkNormal, rkPoint);
+	}
+	//-----------------------------------------------------------------------
+	Plane::Plane (const Vec3& rkPoint0, const Vec3& rkPoint1,
+		const Vec3& rkPoint2)
+	{
+		redefine(rkPoint0, rkPoint1, rkPoint2);
+	}
+	//-----------------------------------------------------------------------
+	Real Plane::getDistance (const Vec3& rkPoint) const
+	{
+		return normal.dot(rkPoint) + d;
+	}
+
+	//-----------------------------------------------------------------------
+	void Plane::redefine(const Vec3& rkPoint0, const Vec3& rkPoint1,
+		const Vec3& rkPoint2)
+	{
+		Vec3 kEdge1 = rkPoint1 - rkPoint0;
+		Vec3 kEdge2 = rkPoint2 - rkPoint0;
+		normal = kEdge1.cross(kEdge2);
+		normal.normalise();
+		d = -normal.dot(rkPoint0);
+	}
+	//-----------------------------------------------------------------------
+	void Plane::redefine(const Vec3& rkNormal, const Vec3& rkPoint)
+	{
+		normal = rkNormal;
+		d = -rkNormal.dot(rkPoint);
+	}
+	//-----------------------------------------------------------------------
+	Vec3 Plane::projectVector(const Vec3& p) const
+	{
+		// We know plane normal is unit length, so use simple method
+		Mat3 xform;
+		xform[0][0] = 1.0f - normal.x * normal.x;
+		xform[0][1] = -normal.x * normal.y;
+		xform[0][2] = -normal.x * normal.z;
+		xform[1][0] = -normal.y * normal.x;
+		xform[1][1] = 1.0f - normal.y * normal.y;
+		xform[1][2] = -normal.y * normal.z;
+		xform[2][0] = -normal.z * normal.x;
+		xform[2][1] = -normal.z * normal.y;
+		xform[2][2] = 1.0f - normal.z * normal.z;
+		return xform * p;
+
+	}
+	//-----------------------------------------------------------------------
+	Real Plane::normalise(void)
+	{
+		Real fLength = normal.length();
+
+		// Will also work for zero-sized vectors, but will change nothing
+		if (fLength > 1e-08f)
+		{
+			Real fInvLength = 1.0f / fLength;
+			normal *= fInvLength;
+			d *= fInvLength;
+		}
+
+		return fLength;
+	}
 
 }
